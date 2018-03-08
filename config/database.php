6763fcb812c0c2c97dd076a5f6174f76cd65865a
RTAndrew <anderson_rodax@hotmail.com>
2018-03-08 23:25:01 +0100

    OpenShift DB Pro

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '172.30.170.15'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'default'),
             'username' => env('DB_USERNAME', 'laravel'),
