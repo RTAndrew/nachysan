8fa36d031766ad2140885fb732e18517e2db79d3
RTAndrew <anderson_rodax@hotmail.com>
2018-03-08 19:53:04 +0100

    OpenShift DB

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => getenv('OPENSHIFT_MYSQL_DB_HOST'),
-            'port' => getenv('OPENSHIFT_MYSQL_DB_PORT'),
-            'database' => getenv('OPENSHIFT_GEAR_NAME'),
-            'username' => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
-            'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'default'),
+            'username' => env('DB_USERNAME', 'laravel-mysql-persistent'),
+            'password' => env('DB_PASSWORD', 'laravel-mysql-persistent'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
