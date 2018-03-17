6ec2bdb8c91b1f756d004c4b0950fd018d62706f
RTAndrew <anderson_rodax@hotmail.com>
2018-03-17 04:11:02 +0100

    Fortra

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'laravel-sg6o.mysql.us1.frbit.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'nachysan'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' => env('DB_DATABASE', 'laravel-sg6o'),
+            'username' => env('DB_USERNAME', 'laravel-sg6o'),
+            'password' => env('DB_PASSWORD', 'XJMbRs=heBUahIAwFfsq8=H2'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
