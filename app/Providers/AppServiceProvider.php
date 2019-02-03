26e0675da18391cf81ff06dbf7717b4ba77d2ed7
RTAndrew <anderson_rodax@hotmail.com>
2019-02-03 22:03:41 +0100

    Laravel Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,32 +25,32 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
 
-// VIEW COMPOSER
-    View::composer(['*'], function($view){
+    // VIEW COMPOSER
+        View::composer(['*'], function($view){
 
-        //Passar as categorias dos productos
-        $categorias = Categoria::with('sub_categoria')->get();
-        
-        //Passar a lista dos paises
-        $paises = info_pais::with('contacto')->get();
-        
-        //Passar as redes sociais
-        $rede_social = rede_social::all();
-            // Como é impossivel fazer o loop foreach na mesma variavel
-            // Duplica-se esta segunda variavel para o footer da versao mobile
-            $rede_social_2 = rede_social::all();
+            //Passar as categorias dos productos
+            $categorias = Categoria::with('sub_categoria')->get();
+            
+            //Passar a lista dos paises
+            $paises = info_pais::with('contacto')->get();
+            
+            //Passar as redes sociais
+            $rede_social = rede_social::all();
+                // Como é impossivel fazer o loop foreach na mesma variavel
+                // Duplica-se esta segunda variavel para o footer da versao mobile
+                $rede_social_2 = rede_social::all();
 
 
-        $num_contacto = num_contacto::where('descricao_tipo_id', 1)->first();
+            $num_contacto = num_contacto::where('descricao_tipo_id', 1)->first();
 
-        $view
-        ->with('paises', $paises)
-        ->with('rede_social', $rede_social)
-        ->with('rede_social_2', $rede_social_2)
-        ->with('num_contacto', $num_contacto)
-        ->with('categorias', $categorias);
+            $view
+            ->with('paises', $paises)
+            ->with('rede_social', $rede_social)
+            ->with('rede_social_2', $rede_social_2)
+            ->with('num_contacto', $num_contacto)
+            ->with('categorias', $categorias);
 
-    });
+        });
 
 
     }
